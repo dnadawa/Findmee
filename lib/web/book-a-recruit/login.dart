5c7e846b2c5068f36608e22efe4ffdb60791604b
Sanjuwa <sanjulahasaranga0@gmail.com>
2021-11-05 16:56:31 +0530

    Configurations

--- lib/web/book-a-recruit/login.dart
+++ lib/web/book-a-recruit/login.dart
@@ -140,7 +140,7 @@ class _LoginWebCompanyState extends State<LoginWebCompany> {
                         }
                       }
                       on FirebaseAuthException catch(e){
-                        if (e.code == 'user-not-foun') {
+                        if (e.code == 'user-not-fou') {
                           pd.hide();
                           MessageDialog.show(context: context, text: 'Ingen bruger fundet til den e-mail');
                         }
